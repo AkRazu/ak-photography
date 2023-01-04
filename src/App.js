c396542212d752b14e88de8122b98829292f4cc7
Ak Razu <fbook3603@gmail.com>
2023-01-05 01:48:37 +0600

    footer section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Hero from './components/Header/Hero/Hero';
 import Navigation from './components/Header/Navigation/Navigation';
 import './App.css';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <Hero/>
-      
+      <Footer/>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,33 @@
-import React from 'react';
-
+import React from "react";
+import logo from "../../images/logo3.png";
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <div className=" bg-[#FFF9F4]">
+      <div className="container">
+        <div className="grid md:grid-cols-2">
+          <div>
+            <div>
+              <img className="w-5/12 mx-auto" src={logo} alt="" />
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <div >
+              <p style={{letterSpacing:"5px"}} className="text-center">Find Us on</p>
+              <p>
+              <i class="mx-2 text-3xl fa-brands fa-facebook"></i>
+              <i class="mx-2 text-3xl fa-brands fa-square-twitter"></i>
+              <i class="mx-2 text-3xl fa-brands fa-instagram"></i>
+              <i class="mx-2 text-3xl fa-brands fa-linkedin"></i>
+              </p>
+            </div>
+          </div>
         </div>
-    );
+        <p>
+                <small>&copy; ALL RIGHT RESERVED 2023</small>
+              </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
